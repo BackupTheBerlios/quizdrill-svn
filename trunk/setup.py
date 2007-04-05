87fd29d97a2d062047a23426eed9434277e1bbb3
schmalhof <schmalhof@45309d0a-e127-0410-9af1-d5a3da9e9ae9>
2007-04-05 20:57:08 +0000

    Use setuptools to also support eggs
    * setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -18,7 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='quizdrill', 
         version='0.2.0',
