159e75b0a6b3e38dc3efe627787ef40e6caa40ac
schmalhof <schmalhof@45309d0a-e127-0410-9af1-d5a3da9e9ae9>
2007-04-09 19:33:50 +0000

    Initial functional version of SaDrill
    * src/SaDrill.py

--- trunk/src/SaDrill.py
+++ trunk/src/SaDrill.py
@@ -20,50 +20,105 @@
 
 class SaDrill:
     """
-    Simple API for .drill.
+    Simple API for .drill or a .build of it.
     Accessing Quizdrill files similar to SAX (Simple API for XML).
 
-    Note: This is not functional yet, nor is it used anywhere (yet).
+    Note: This is not used anywhere yet.
     """
 
+    def __init__(self, head_tag_dict={}, build_tag_dict={}):
+        self.head_tag_dict = { 
+                "language" : self.on_default_head_tag, 
+                "question" : self.on_default_head_tag, 
+                "type" : self.on_default_head_tag,
+                "media" : self.on_default_head_tag,
+                "generator" : self.on_default_head_tag 
+                }
+        self.build_tag_dict = { 
+                "build_to" : self.on_default_build_tag, 
+                "builder" : self.on_default_build_tag, 
+                "filter" : self.on_default_build_tag
+                }
+        self.head_tag_dict.update(head_tag_dict)
+        self.build_tag_dict.update(build_tag_dict)
+
     def parse(self, drill_file):
-        self.drill_file = drill_file
+        """
+        Parse a .drill or .build file.
+        """
+        self.current_drill_file = drill_file
+        head_tag_dict = self.head_tag_dict
+        build_tag_dict = self.build_tag_dict
+
         for i, line in enumerate(f.readlines()):
             line = line.strip()
             if len(line) > 0:
-                if line[0] == '#':
-                    continue
-                elif line[0] == '!':
+                type = line[0]
+                if type == '#':
+                    on_comment(line, None, None, type)
+                elif type == '!':
                     colon = line.index(":")
                     tag = line[1:colon]
                     word_pair = [ w.strip() for w in line[colon+1:].split("=")]
-                    if tag in tag_dict:
-                        tag_dict[tag](word_pair)
+                    if tag in head_tag_dict:
+                        head_tag_dict[tag](word_pair)
                     else:
-                        print _('Warning: unknown tag "%s".') % tag
-                elif line[0] == '[':
+                        print _('Warning: unknown head-tag "%s".') % tag
+                elif type == '[':
                     line = line[1:-1]
                     word_pair = [ w.strip() for w in line.split("=", 1) ]
                     if len(word_pair) < 2:
                         word_pair.append("")
-                    column = []; column.extend(word_pair)
-                    column.append(True)
-                    section = self.treestore.append(None, column)
-                elif line[0] == '$':
-                    line = line[1:-1]
+                    on_section(line, word_pair, None, type)
+                elif type == '$':
+                    colon = line.index(":")
+                    tag = line[1:colon]
+                    word_pair = [ w.strip() for w in line[colon+1:].split("=")]
+                    if tag in build_tag_dict:
+                        build_tag_dict[tag](word_pair)
+                    else:
+                        print _('Warning: unknown build-tag "%s".') % tag
                 else:
+                    type = ''
                     word_pair = [ w.strip() for w in line.split("=") ]
                     assert len(word_pair) == 2, 'Fileformaterror in "%s": \
                             Not exactly one "=" in line %s' % ( file, i+1 )
-                    quizlist.append(word_pair)
-                    column = []; column.extend(word_pair)
-                    column.append(True)
-                    self.treestore.append(section, column)
+                    on_question(line, word_pair, None, type)
         f.close()
-        return quizlist
+        self.current_drill_file = None
 
-    def startSection(self, name):
+    def on_comment(as_text, word_pair=None, tag=None, type='#'):
+        """
+        Processes a comment line of an .drill or .build file. Overload this
+        method so something is actually done.
+        """
         pass
 
-    def endSection(self, name):
+    def on_section(as_text, word_pair, tag=None, type='['):
+        """
+        Processes a section line of an .drill or .build file. Overload this
+        method so something is actually done.
+        """
         pass
+
+    def on_question(as_text, word_pair, tag=None, type=''):
+        """
+        Processes a question-answer line of an .drill or .build file. Overload 
+        this method so something is actually done.
+        """
+        pass
+
+    def on_default_head_tag(as_text, word_pair, tag, type='!'):
+        """
+        Processes a header line of an .drill or .build file. Overload this
+        method so something is actually done.
+        """
+        pass
+
+    def on_default_build_tag(as_text, word_pair, tag, type='$'):
+        """
+        Processes a builder line of an .drill or .build file. Overload this
+        method so something is actually done.
+        """
+        pass
+
