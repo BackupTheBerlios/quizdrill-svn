dd1baf1f20a5d33cea53064a4de5c7ceeb2f5732
schmalhof <schmalhof@45309d0a-e127-0410-9af1-d5a3da9e9ae9>
2007-04-19 13:35:06 +0000

    Merged gettext-mo-files.py into setup.py.
    * gettext-mo-files.py: Removed.
    
    * setup.py
      (make_mo_gettext): Copied from gettext-mo-files.py.
      (make_setup): New. What was previously called when setup.py was run.
      (__main__): Switch to call one of the two. Keyword for make_mo_gettext is
          "gettext-mo".

--- trunk/setup.py
+++ trunk/setup.py
@@ -19,46 +19,97 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 from setuptools import setup
+import sys
 
-setup(name='quizdrill', 
-        version='0.2.0',
-        # prevents *_scripts from working when installed by debian package
-        #install_requires=['pygtk > 2.0'],   # much higher pygtk than written; 
-        license='GNU General Public License',
-        platforms=['any'],
-        # descriptions should be kept in sync with debian/control,
-        # the berlios project page, the wiki-homepage and (future) freshmeat.
-        description='A learning-by-testing to excess program.',
-        long_description="""A learning-by-testing program to learn quickly, 
-            mostly memorizing tasks like vocabulary. Quizdrill supports
-            multiple choice, simple quiz as well as flashcard testing.
-            """,
-        author='Adam Schmalhofer',
-        author_email='schmalhof@users.berlios.de',
-        url='http://quizdrill.berlios.de/',
-        package_dir={'quizdrill': 'src'},
-        packages=['quizdrill'],
-        package_data={'quizdrill': ['data/quizdrill.glade']},
-        data_files=[('quizzes', ['quizzes/deu-fra.drill', 
-            'quizzes/eng-fra.drill', 'quizzes/eng-jpn.drill', 
-            'quizzes/eng-jpn_romaji.drill', 'quizzes/eng-svd.drill', 
-            'quizzes/fra_verb.drill']),
-            ('po', ['po/de.po']),
-            ('doc', ['README', 'TODO', 'GPL-2', 'Changes'])],
-        entry_points={
-            'console_scripts': [ 'quiz_builder = quizdrill.builder:build' ],
-            'gui_scripts': [ 'quizdrill = quizdrill.quizdrill:main' ]
-            },
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: X11 Applications :: Gnome',
-            'Intended Audience :: Education',
-            'Intended Audience :: End Users/Desktop',
-            'License :: OSI Approved :: GNU General Public License (GPL)',
-            'Natural Language :: English',
-            'Natural Language :: German',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Education :: Testing'
-            ]
-        )
+from os import listdir, spawnlp, P_WAIT, makedirs
+from os.path import basename, normpath
+from glob import glob
+
+def make_mo_gettext():
+    """
+    Calls 'msgfmt' from GNU gettext to genearte object files (.mo) from
+    the translation files (.po).
+
+    Note: As this function usese the $PATH variable (with spawnlp) it doesn't
+      work under Windows.
+    """
+    print "Generating gettext mo files: "
+    po_files = 'po/*.po'
+    mo_base_dir = 'locale/%s/LC_MESSAGES/'
+    conv_program = 'msgfmt'
+
+    for lang_file in glob(po_files):
+        language = basename(lang_file)[:-3]
+        mo_dir = mo_base_dir % language
+        print language + " "
+        try:
+            makedirs(mo_dir)
+        except OSError, inst:
+            if inst.strerror != 'File exists':
+                print 'Warning: ', inst.file, inst.strerror, 'ignoring.'
+        # normalize path for windows #
+        lang_file_norm = normpath(lang_file)
+        mo_dir_norm = normpath(mo_dir)
+        #
+        mo_file = mo_dir_norm + "/quizdrill.mo"
+        #print conv_program, lang_file, "-o", mo_file    # debugging
+        spawnlp(P_WAIT, conv_program, conv_program, lang_file_norm, "-o", 
+                mo_file)
+    print "done"
+
+def make_setup():
+    """
+    The setuptools setup packaging for eggs, Python Cheese Shop 
+    registration and distutils install.
+    """
+    setup(name='quizdrill', 
+            version='0.2.0',
+            # prevents *_scripts from working when installed by debian package
+            # much higher minimum pygtk version is needed than written
+            #install_requires=['pygtk > 2.0'],
+            license='GNU General Public License',
+            platforms=['any'],
+            # descriptions should be kept in sync with debian/control,
+            # the berlios project page, the wiki-homepage and 
+            # (in the future) freshmeat.
+            description='A learning-by-testing to excess program.',
+            long_description="""A learning-by-testing program to learn quickly, 
+                mostly memorizing tasks like vocabulary. Quizdrill supports
+                multiple choice, simple quiz as well as flashcard testing.
+                """,
+            author='Adam Schmalhofer',
+            author_email='schmalhof@users.berlios.de',
+            url='http://quizdrill.berlios.de/',
+            package_dir={'quizdrill': 'src'},
+            packages=['quizdrill'],
+            package_data={'quizdrill': ['data/quizdrill.glade']},
+            data_files=[('quizzes', ['quizzes/deu-fra.drill', 
+                'quizzes/eng-fra.drill', 'quizzes/eng-jpn.drill', 
+                'quizzes/eng-jpn_romaji.drill', 'quizzes/eng-svd.drill', 
+                'quizzes/fra_verb.drill']),
+                ('po', ['po/de.po']),
+                ('doc', ['README', 'TODO', 'GPL-2', 'Changes'])],
+            entry_points={
+                'console_scripts': [ 'quiz_builder = quizdrill.builder:build' ],
+                'gui_scripts': [ 'quizdrill = quizdrill.quizdrill:main' ]
+                },
+            classifiers=[
+                'Development Status :: 4 - Beta',
+                'Environment :: X11 Applications :: Gnome',
+                'Intended Audience :: Education',
+                'Intended Audience :: End Users/Desktop',
+                'License :: OSI Approved :: GNU General Public License (GPL)',
+                'Natural Language :: English',
+                'Natural Language :: German',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Topic :: Education :: Testing'
+                ]
+            )
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'gettext-mo':
+        make_mo_gettext()
+    else:
+        make_setup()
